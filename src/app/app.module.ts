e246ce2343c3513843508207fc1b0fd0c79069ec
Jhemeson Silva Mota <jhemesonmotta@gmail.com>
2019-10-22 23:02:25 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SideBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
