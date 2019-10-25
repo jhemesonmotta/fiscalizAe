644dbfe7307bacf6edcd598634cf1c2c940605cf
unknown <IMGADMIN@L-435604159.wipro.com>
2019-10-25 15:39:11 -0300

    versão do ng atualizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PartialGraficosComponent } from './components/dashboard/partials/partial-graficos/partial-graficos.component';
 import { PartialListasComponent } from './components/dashboard/partials/partial-listas/partial-listas.component';
 import { PartialEstatisticasRapidasComponent } from './components/dashboard/partials/partial-estatisticas-rapidas/partial-estatisticas-rapidas.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,10 @@ import { PartialEstatisticasRapidasComponent } from './components/dashboard/part
     PartialEstatisticasRapidasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    NgxChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
