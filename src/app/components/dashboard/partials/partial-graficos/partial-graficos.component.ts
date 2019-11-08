3738ad1d0be2669ca06b2fa3d8a1253ceb374bbe
unknown <IMGADMIN@L-435604159.wipro.com>
2019-11-07 22:04:07 -0300

    acumulado de compras governamentais

--- src/app/components/dashboard/partials/partial-graficos/partial-graficos.component.ts
+++ src/app/components/dashboard/partials/partial-graficos/partial-graficos.component.ts
@@ -4,6 +4,7 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { dataset } from './mocked-data';
 import { YearMedianService } from 'src/app/services/yearMedianService';
 import { BarChartItem } from 'src/app/models/barChartItem';
+import { YearAccumulatedService } from 'src/app/services/yearAccumulatedService';
 
 @Component({
   selector: 'app-partial-graficos',
@@ -18,13 +19,19 @@ export class PartialGraficosComponent implements OnInit {
   viewcol6: any[] = [500, 300];
   viewcol12: any[] = [1024, 300];
   yearMedianService: YearMedianService = new YearMedianService();
+  yearAccumulatedService: YearAccumulatedService = new YearAccumulatedService();
   medianaPorAno: Array<BarChartItem> = new Array<BarChartItem>();
+  acumuladoPorAno: Array<BarChartItem> = new Array<BarChartItem>();
 
   constructor() {
     // Object.assign(this, { dataset });
     this.medianaPorAno = this.yearMedianService.retornaListaMedianaPorAno();
     console.log('medianaPorAno');
     console.log(this.medianaPorAno);
+
+    this.acumuladoPorAno = this.yearAccumulatedService.retornaListaAcumuladoPorAno();
+    console.log('acumuladoPorAno');
+    console.log(this.acumuladoPorAno);
   }
 
   ngOnInit() {
